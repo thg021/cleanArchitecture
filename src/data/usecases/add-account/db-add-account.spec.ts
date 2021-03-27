67f93dce79cc6bf8f8dbe356fb686fd8c9f0aa33
Thiago Silva <thiago.silva@hotmail.com>
2021-03-27 11:55:59 -0300

    test: ensure DbAddAccount return an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -22,7 +22,7 @@ const makeAddAccountRepository = (): AddAccountRepository => {
       const fakeAccount = {
         id: 'valid_id',
         name: 'valid_name',
-        email: 'valid_email',
+        email: 'valid_mail@mail.com',
         password: 'hashed_password',
       };
       return new Promise(resolve => resolve(fakeAccount));
@@ -112,5 +112,22 @@ describe('DbAddAccount Usecase', () => {
 
     const promise = sut.add(accountData);
     await expect(promise).rejects.toThrow();
+  });
+
+  test('should return an account on success', async () => {
+    const { sut } = makeSut();
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_mail@mail.com',
+      password: 'valid_password',
+    };
+
+    const account = await sut.add(accountData);
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_mail@mail.com',
+      password: 'hashed_password',
+    });
   });
 });
